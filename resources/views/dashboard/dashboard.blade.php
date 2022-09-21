bdddc9391370c36c46980f61c586116345e4153b
SuperTurk38 <d280587@edu.rocwb.nl>
2022-09-21 10:09:51 +0200

    Base elements for the dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,5 +1,71 @@
 @extends('dashboard.base')
 
 @section('content')
-    <h1 class="none">test</h1>
+    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <table class="table">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-sm">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title that wraps to a new line</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <table class="table">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/dashboard/mijnprofiel.blade.php
+++ resources/views/dashboard/mijnprofiel.blade.php
@@ -1,1 +1,15 @@
-@extends('base')
+@extends('dashboard.base')
+@section('content')
+<div class="container">
+    <h1>Mijn Account</h1>
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" name="name">
+    </div>
+    <div class="form-group">
+        <label for="">Achternaam</label>
+        <input type="text" name="achternaam">
+    </div>
+</div>
+
+@endsection
